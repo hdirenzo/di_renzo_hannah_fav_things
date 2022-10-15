a140f27e8fe8b6c66d68c1d7cbe4f22a2a469d03
Hannah Di Renzo <hdirenzo@outlook.com>
2022-10-14 19:39:00 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Grab all the movie buttons by their class name
+const buttons = document.querySelectorAll('.movie-button')
+
+for (const button of buttons) {
+    button.addEventListener('click', handleMovieClick)
+}
+
+function handleMovieClick(event) {
+
+    const movieTitle = event.target.textContent
+
+
+    fetch('json/data.json')
+        // Fetch finished (resolved), do this with the result
+        .then(function (data) {
+            return data.json()
+        })
+
+        .then(function (movies) {
+            // Dynamically create some HTML content with the data 
+            const movie = movies[movieTitle]
+            const movieCardEl = createMovieCard(movie)
+            console.log(movieCardEl)
+        })
+
+        .catch(function (error) {
+            alert('Error: ' + error.message)
+        })
+}
+
+/**
+ * Creates a movie card and adds it
+ * 
+ * @param {Object} movie
+ * @returns {HTMLDivElement}
+ */
+function createMovieCard(movie) {
+    const movieCard = document.createElement('div')
+    movieCard.innerHTML =
+        `<div class="movie-card">
+            <h2 class="movie-card__title">${movie.name}</h2>
+            <p>${movie.desc}</p>
+        </div>`
+
+    document.body.appendChild(movieCard)
+
+    return movieCard
+}
